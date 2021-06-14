8ea37d36ce7f27ccb7923f2e09fab1ff49a9ce29
OM <omprakash.eee18@itbhu.ac.in>
2021-06-14 11:25:46 +0530

    modify-script-component

--- src/Component/ScriptComponent/ScriptComponent.js
+++ src/Component/ScriptComponent/ScriptComponent.js
@@ -1,48 +1,87 @@
-import React from 'react' ;
-import './ScriptComponent.css'
-const ScriptComponent =(props) =>{
-   const {AlertType} =props ;
-    return (
-        <div className="col-lg-6 third">
-        <h1 id="Alert-heading" className="Alert-heading">{AlertType}</h1>
-        <form>
-          <div className="form-group">
-          <label>Script 1</label> 
-            <input
-              type="text"
-              className="form-control"
-              placeholder="script"
-            />
-          </div>
-          <div className="form-group">
-            <label >Script 2</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="script"
-            />
-          </div>
-          <div className="form-group">
-            <label >Script 3</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="script"
-            />
-          </div>
-          <div className="form-group">
-            <label >Script 4</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="script"
-            />
-          </div>
-          <button className=" btn script-btn btn-primary" type="submit">
-            Add
-          </button>
-        </form>
-      </div>
-    ) ;
-}
-export default ScriptComponent ;
+import React, { useState } from "react";
+import "./ScriptComponent.css";
+import AddOutlinedIcon from '@material-ui/icons/AddCircleOutline';
+
+const ScriptComponent = (props) => {
+  const [ScriptData, setScriptData] = useState({
+    Script1: "",
+    Script2: "",
+    Script3: "",
+    Script4: "",
+  });
+
+  const updateScriptData = (event) =>
+    setScriptData({
+      ...ScriptData,
+      [event.target.name]: event.target.value,
+    });
+
+  const { Script1, Script2, Script3, Script4 } = ScriptData;
+
+  function handleSubmit(event) {
+    console.log(ScriptData);
+    event.preventDefault();
+  }
+
+  const { AlertType } = props;
+
+  return (
+    <div className="col-lg-6 third">
+      <h1 id="Alert-heading" className="Alert-heading">
+        {AlertType}
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Script 1</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder=""
+            value={Script1}
+            name="Script1"
+            onChange={(e) => updateScriptData(e)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Script 2</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder=""
+            value={Script2}
+            name="Script2"
+            onChange={(e) => updateScriptData(e)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Script 3</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder=""
+            value={Script3}
+            name="Script3"
+            onChange={(e) => updateScriptData(e)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Script 4</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder=""
+            value={Script4}
+            name="Script4"
+            onChange={(e) => updateScriptData(e)}
+          />
+        </div>
+        <button className=" btn  btn-primary script-btn" type="submit">
+        Add  <span className="plus-icon"><AddOutlinedIcon fontSize="small" /></span>
+        </button>
+         
+      </form>
+      
+    </div>
+  );
+};
+export default ScriptComponent;
