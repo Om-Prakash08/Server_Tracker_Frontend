5b2dc6e06d3b36e296765f9efecabd373dbc4d05
Om Prakash <44462272+Om-Prakash08@users.noreply.github.com>
2021-06-23 12:37:06 +0530

    Update fetchServerList.js

--- src/Component/ServiceComponent/fetchServerList.js
+++ src/Component/ServiceComponent/fetchServerList.js
@@ -1,54 +1,147 @@
-import axios from "axios";
-const getServiceList = async (setServiceList,token) => {
-  try {
-    const resp = await axios.get(`${process.env.REACT_APP_BACKEND_API_URL}/service/`, {
-      headers: {
-        "x-auth-token": token ,
-      },
-    });
-    console.log(resp.data) ;
-    setServiceList(resp.data);
-  } catch (err) {
-    console.error(err);
-  }
-};
+import React, { useState, Fragment, useEffect } from "react";
+import "./ScriptComponent.css";
+import Button from "@material-ui/core/Button";
+import ScriptErrorComponent from "./ScriptErrorComponent";
+import DeleteIcon from "@material-ui/icons/Delete";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import IconButton from "@material-ui/core/IconButton";
+import { getScriptValue, sendScriptValue } from "./HandleScriptApi";
 
-const getGroupList = async (setGroupList, service,token) => {
-  const name = service.serviceName;
-  try {
-    const resp = await axios({
-      method: "POST",
-      url: `${process.env.REACT_APP_BACKEND_API_URL}/serverGrp/`,
-      data: {
-        serviceName:name,
-      },
-      headers: {
-        "x-auth-token": token ,
-      },
-    });
-    setGroupList(resp.data);
-  } catch (err) {
-    console.error(err);
-  }
-};
+const ScriptComponent = (props) => {
+  const { serviceAlertData, AlertType, token } = props;
+  const [sending, SetSending] = useState(false);
+  const [Apierror, setApiError] = useState(false);
+  const [emptyAlertError, setEmptyAlertError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [inputFields, setInputFields] = useState([
+    { script: "" },
+    { script: "" },
+    { script: "" },
+    { script: "" },
+    { script: "" },
+  ]);
 
-const getServerList = async (setServerList, group,token) => {
-  const name = group.serverGrpName;
-  try {
-    const resp = await axios({
-      method: "POST",
-      url: `${process.env.REACT_APP_BACKEND_API_URL}/server/`,
-      data: {
-        serverGrpName:name,
-      },
-      headers: {
-        "x-auth-token": token ,
-      },
-    });
-    setServerList(resp.data);
-  } catch (err) {
-    console.error(err);
-  }
-};
+  useEffect(() => {
+    if (serviceAlertData.alertName && serviceAlertData.serverName)
+      getScriptValue(
+        setInputFields,
+        serviceAlertData.serverName,
+        serviceAlertData.alertName,
+        token
+      );
+    else{
+      setInputFields([{ script: "" }]);
+    }
+    setSuccess(false);
+    // eslint-disable-next-line
+  }, [serviceAlertData.alertName,serviceAlertData.serverName]);
 
-export { getServiceList, getGroupList, getServerList };
+  const handleAddFields = (index) => {
+    const values = [...inputFields];
+    values.splice(index + 1, 0, { script: "" });
+    setInputFields(values);
+  };
+
+  const handleRemoveFields = (index) => {
+    const values = [...inputFields];
+    values.splice(index, 1);
+    setInputFields(values);
+  };
+
+  const handleInputChange = (index, event) => {
+    const values = [...inputFields];
+    values[index].script = event.target.value;
+    setInputFields(values);
+  };
+
+  const handleSubmit = (e) => {
+    if (AlertType) {
+      const data = {
+        alertName: serviceAlertData.alertName,
+        serverName: serviceAlertData.serverName,
+        scripts: inputFields,
+      };
+      sendScriptValue(SetSending, setApiError, setSuccess, data, token);
+      setEmptyAlertError(false);
+    } else {
+      setEmptyAlertError(true);
+    }
+    e.preventDefault();
+  };
+
+  return (
+    <div className="col-lg-6 third">
+      <div className="Script-heading">
+        <h1 id="Alert-heading" className="Alert-heading">
+          Script
+        </h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="script-form-div">
+          <div className="form-row">
+            {inputFields.map((inputField, index) => (
+              <Fragment key={`${index}`}>
+                <div className="form-group ">
+                  <textarea
+                    type="text"
+                    cols="76"
+                    rows="2"
+                    className={
+                      index !== 0 && index !== inputFields.length - 1
+                        ? "textarea-input"
+                        : ""
+                    }
+                    id={index}
+                    name="script"
+                    placeholder={"Script " + JSON.stringify(index + 1)}
+                    value={inputField.script}
+                    onChange={(event) => handleInputChange(index, event)}
+                    autoComplete="off"
+                    data-gramm_editor="false"
+                  />
+                  <IconButton
+                    aria-label="delete"
+                    className="delete-script-btn"
+                    type="button"
+                    onClick={() => handleRemoveFields(index)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                  <Fab
+                    color="primary"
+                    aria-label="add"
+                    size="small"
+                    className="add-script-btn"
+                    type="button"
+                    onClick={() => handleAddFields(index)}
+                  >
+                    <AddIcon />
+                  </Fab>
+                </div>
+              </Fragment>
+            ))}
+          </div>
+        </div>
+        <Button
+          variant="contained"
+          size="medium"
+          color="primary"
+          type="submit"
+          className="script-submit-btn"
+          disabled={sending}
+        >
+          Save
+        </Button>
+      </form>
+      <ScriptErrorComponent
+        Apierror={Apierror}
+        emptyAlertError={emptyAlertError}
+        success={success}
+        AlertType={AlertType}
+        className="ScriptError"
+      />
+    </div>
+  );
+};
+export default ScriptComponent;
